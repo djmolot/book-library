8f58fb78b8e2387e1ff5e1aa54ae2359b54fc6b4
djmolot <djmolot@gmail.com>
2022-12-02 16:29:42 +0100

    deleted unnecessary files, renamed file application.properties, corrected class WelcomeController

--- src/main/java/company/name/library/controllers/WelcomeController.java
+++ src/main/java/company/name/library/controllers/WelcomeController.java
@@ -1,9 +1,6 @@
 package company.name.library.controllers;
 
-import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.time.LocalDate;
@@ -14,10 +11,8 @@ import java.util.Map;
 @RestController
 public class WelcomeController {
     @GetMapping("/api/v1/library/welcome")
-    //@RequestMapping(value = "/api/v1/library/welcome", method = RequestMethod.GET,
-            //produces = MediaType.APPLICATION_JSON_VALUE)
-    public Map<String, Object> sayWelcome() {
-        Map<String, Object> responseMap = new LinkedHashMap<>();
+    public Map<String, String> sayWelcome() {
+        Map<String, String> responseMap = new LinkedHashMap<>();
         responseMap.put("message", "Welcome to the library!");
         responseMap.put("currentDate", LocalDate.now().format(DateTimeFormatter.ofPattern("dd.MM.yyyy")));
         return responseMap;
