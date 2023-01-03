ec26469901a49c0d6adb02874d59167ebd1a9a92
djmolot <djmolot@gmail.com>
2023-01-03 18:14:08 +0100

    added annotations @Data in classes Book & Reader

--- src/main/java/company/name/library/entities/Reader.java
+++ src/main/java/company/name/library/entities/Reader.java
@@ -1,58 +1,12 @@
 package company.name.library.entities;
 
+import lombok.Data;
 import java.util.List;
 
+@Data
 public class Reader {
     private Long id;
     private String name;
     //@OneToMany
     private List<Book> books;
-
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public List<Book> getBooks() {
-        return books;
-    }
-
-    public void setBooks(List<Book> books) {
-        this.books = books;
-    }
-
-    @Override
-    public boolean equals(Object obj) {
-        if (obj == this) {
-            return true;
-        }
-        if (obj == null) {
-            return false;
-        }
-        if (obj.getClass().equals(Reader.class)) {
-            Reader casted = (Reader) obj;
-            if (casted.id == null) {
-                return this.name.equals(casted.name);
-            } else {
-                return this.id.equals(casted.id) && this.name.equals(casted.name);
-            }
-        }
-        return false;
-    }
-
-    @Override
-    public String toString() {
-        return (id + ". " + name);
-    }
 }
