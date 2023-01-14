9edcbb2b11cc9f810231a162efdb7f82b597676e
djmolot <djmolot@gmail.com>
2023-01-14 13:13:11 +0100

    added @NotNull for fields author & title

--- src/main/java/company/name/library/entities/Book.java
+++ src/main/java/company/name/library/entities/Book.java
@@ -1,5 +1,6 @@
 package company.name.library.entities;
 
+import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Size;
 import lombok.Data;
 import java.util.Optional;
@@ -9,10 +10,12 @@ public class Book {
 
     private Long id;
 
+    @NotNull(message = "Please provide an author")
     @Size(min = 3, max = 50, message
             = "Author must be between 3 and 50 characters")
     private String author;
 
+    @NotNull(message = "Please provide a title")
     @Size(min = 10, max = 255, message
             = "Title must be between 10 and 255 characters")
     private String title;
