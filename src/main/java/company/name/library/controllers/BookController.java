fb9e7ffed6c73a94f5fe1aa158fdf4c825dffe79
djmolot <djmolot@gmail.com>
2023-05-06 10:46:14 +0200

    added descriptions in BookController & ReaderController

--- src/main/java/company/name/library/controllers/BookController.java
+++ src/main/java/company/name/library/controllers/BookController.java
@@ -1,8 +1,16 @@
 package company.name.library.controllers;
 
 import company.name.library.entities.Book;
+import company.name.library.entities.ErrorResponse;
 import company.name.library.entities.Reader;
 import company.name.library.service.LibraryService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
 import jakarta.validation.constraints.Positive;
 import lombok.RequiredArgsConstructor;
@@ -20,38 +28,93 @@ import java.util.List;
 
 @RequiredArgsConstructor
 @RestController
-@RequestMapping("/api/v1/library/books")
+@RequestMapping("/api/v1/books")
 @Validated
+@Tag(name = "Book Controller", description = "Endpoints for managing books in the library")
 public class BookController {
     private final LibraryService libraryService;
 
+    @Operation(summary = "Get all books present in the library")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Books was returned successfully",
+                    content = { @Content(mediaType = "application/json",
+                            schema = @Schema(implementation = List.class)) })
+    })
     @GetMapping
     public ResponseEntity<List<Book>> showAllBooks() {
         List<Book> books = libraryService.getAllBooks();
         return ResponseEntity.ok(books);
     }
 
+    @Operation(summary = "Add new book to the library")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "201", description = "Book was added to the library",
+                    content = { @Content(mediaType = "application/json",
+                            schema = @Schema(implementation = Book.class)) }),
+            @ApiResponse(responseCode = "400", description = "Invalid book supplied",
+                    content = { @Content(mediaType = "application/json",
+                            schema = @Schema(implementation = ErrorResponse.class)) })
+    })
     @PostMapping
-    public ResponseEntity<Book> addNewBook(@Valid @RequestBody Book book) {
+    public ResponseEntity<Book> addNewBook(
+            @Valid @RequestBody @io.swagger.v3.oas.annotations.parameters.RequestBody(
+                    description = "Book object that needs to be added to the library") Book book) {
         Book bookFromDB = libraryService.addNewBook(book);
         return ResponseEntity.status(HttpStatus.CREATED).body(bookFromDB);
     }
 
+    @Operation(summary = "Borrow book with {id} to reader with {id}")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Book was borrowed to reader",
+                    content = { @Content(mediaType = "application/json",
+                            schema = @Schema(implementation = Book.class)) }),
+            @ApiResponse(responseCode = "400", description = "Invalid parameters supplied to method",
+                    content = { @Content(mediaType = "application/json",
+                            schema = @Schema(implementation = ErrorResponse.class)) })
+    })
     @PostMapping("/{bookId}/readers/{readerId}")
-    public ResponseEntity<Book> borrowBookToReader(@PathVariable("bookId") @Positive Long bookId,
-                                   @PathVariable("readerId") @Positive Long readerId) {
+    public ResponseEntity<Book> borrowBookToReader(
+            @Parameter(description = "id of book to be borrowed")
+            @PathVariable("bookId") @Positive Long bookId,
+            @Parameter(description = "id of reader who borrows a book")
+            @PathVariable("readerId") @Positive Long readerId) {
         Book book = libraryService.borrowBookToReader(bookId, readerId);
         return ResponseEntity.ok(book);
     }
 
+    @Operation(summary = "Return book with id to the library")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Book was returned to the library",
+                    content = { @Content(mediaType = "application/json",
+                            schema = @Schema(implementation = Book.class)) }),
+            @ApiResponse(responseCode = "400", description = "Invalid parameter supplied to method",
+                    content = { @Content(mediaType = "application/json",
+                            schema = @Schema(implementation = ErrorResponse.class)) })
+    })
     @DeleteMapping("/{bookId}/readers")
-    public ResponseEntity<Book> returnBookToLibrary(@PathVariable("bookId") @Positive Long bookId) {
+    public ResponseEntity<Book> returnBookToLibrary(
+            @Parameter(description = "id of book to be returned")
+            @PathVariable("bookId") @Positive Long bookId) {
         Book book = libraryService.returnBookToLibrary(bookId);
         return ResponseEntity.ok(book);
     }
 
+    @Operation(summary = "Get reader of book with id")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Found the reader of book",
+                    content = { @Content(mediaType = "application/json",
+                            schema = @Schema(implementation = Book.class)) }),
+            @ApiResponse(responseCode = "400", description = "Invalid parameter supplied to method",
+                    content = { @Content(mediaType = "application/json",
+                            schema = @Schema(implementation = ErrorResponse.class)) }),
+            @ApiResponse(responseCode = "404", description = "Book is not borrowed by any reader",
+                    content = { @Content(mediaType = "application/json",
+                            schema = @Schema(implementation = ErrorResponse.class)) })
+    })
     @GetMapping("/{bookId}/readers")
-    public ResponseEntity<Reader> showReaderOfBook(@PathVariable("bookId") @Positive Long bookId) {
+    public ResponseEntity<Reader> showReaderOfBook(
+            @Parameter(description = "id of book to find it's reader")
+            @PathVariable("bookId") @Positive Long bookId) {
         return libraryService.getReaderOfBookWithId(bookId)
                 .map(ResponseEntity::ok)
                 .orElseGet(() -> ResponseEntity.notFound().build());
--- src/main/java/company/name/library/controllers/ReaderController.java
+++ src/main/java/company/name/library/controllers/ReaderController.java
@@ -1,8 +1,16 @@
 package company.name.library.controllers;
 
 import company.name.library.entities.Book;
+import company.name.library.entities.ErrorResponse;
 import company.name.library.entities.Reader;
 import company.name.library.service.LibraryService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
 import jakarta.validation.constraints.Positive;
 import lombok.RequiredArgsConstructor;
@@ -15,30 +23,57 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-
 import java.util.List;
 
 @RequiredArgsConstructor
 @RestController
-@RequestMapping("/api/v1/library/readers")
+@RequestMapping("/api/v1/readers")
 @Validated
+@Tag(name = "Reader Controller", description = "Endpoints for managing readers in the library")
 public class ReaderController {
     private final LibraryService libraryService;
 
+    @Operation(summary = "Get all readers registered in the library")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Readers was returned successfully",
+                    content = { @Content(mediaType = "application/json",
+                            schema = @Schema(implementation = List.class)) })
+    })
     @GetMapping
     public ResponseEntity<List<Reader>> showAllReaders() {
         List<Reader> readers = libraryService.getAllReaders();
         return ResponseEntity.ok(readers);
     }
 
+    @Operation(summary = "Add new reader to the library")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "201", description = "Reader was added to the library",
+                    content = { @Content(mediaType = "application/json",
+                            schema = @Schema(implementation = Reader.class)) }),
+            @ApiResponse(responseCode = "400", description = "Invalid reader supplied",
+                    content = { @Content(mediaType = "application/json",
+                            schema = @Schema(implementation = ErrorResponse.class)) })
+    })
     @PostMapping
-    public ResponseEntity<Reader> registerNewReader(@Valid @RequestBody Reader reader) {
+    public ResponseEntity<Reader> registerNewReader(
+            @Valid @RequestBody @io.swagger.v3.oas.annotations.parameters.RequestBody(
+                    description = "Reader object that needs to be added to the library") Reader reader) {
         Reader readerFromDB = libraryService.registerNewReader(reader);
         return ResponseEntity.status(HttpStatus.CREATED).body(readerFromDB);
     }
 
+    @Operation(summary = "Get all books borrowed by reader with {id}")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Found books of the reader",
+                    content = { @Content(mediaType = "application/json",
+                            schema = @Schema(implementation = List.class)) }),
+            @ApiResponse(responseCode = "400", description = "Invalid parameter supplied to method",
+                    content = { @Content(mediaType = "application/json",
+                            schema = @Schema(implementation = ErrorResponse.class)) })
+    })
     @GetMapping("/{readerId}/books")
     public ResponseEntity<List<Book>> showAllBooksBorrowedByReader(
+            @Parameter(description = "id of reader to find his books")
             @PathVariable("readerId") @Positive Long readerId) {
         List<Book> books = libraryService.getAllBooksOfReader(readerId);
         return ResponseEntity.ok(books);
