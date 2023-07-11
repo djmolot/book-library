c8c59db9c0d9c860fcf103480f84592901a7e994
djmolot <djmolot@gmail.com>
2023-07-11 17:18:39 +0200

    removed flyway placeholder & bean flyway from class AppConfig

--- src/main/java/company/name/library/config/AppConfig.java
+++ src/main/java/company/name/library/config/AppConfig.java
@@ -1,22 +1,14 @@
 package company.name.library.config;
 
-import org.flywaydb.core.Flyway;
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.PropertySource;
-
-import javax.sql.DataSource;
-import java.util.Collections;
 
 @Configuration
-@PropertySource("classpath:application.yml")
 public class AppConfig {
     private final String defaultMaxBorrowTimeInDays;
     private final String maxNumberOfBooksToBorrow;
     private final String minAgeOfReaderForRestricted;
 
     public AppConfig() {
-
         this.defaultMaxBorrowTimeInDays = System.getenv("BOOK_LIBRARY__DEFAULT_MAX_BORROW_TIME_IN_DAYS");
         this.maxNumberOfBooksToBorrow = System.getenv("BOOK_LIBRARY__MAXIMUM_NUMBER_OF_BOOKS_FOR_A_READER_TO_BORROW_SIMULTANEOUSLY");
         this.minAgeOfReaderForRestricted = System.getenv("BOOK_LIBRARY__MIMIMUM_AGE_OF_A_READER_FOR_BORROWING_RESTRICTED_BOOKS");
@@ -32,17 +24,6 @@ public class AppConfig {
 
     public String getMinAgeOfReaderForRestricted() {
         return minAgeOfReaderForRestricted;
-    }
-
-    @Bean
-    public Flyway flyway(DataSource dataSource) {
-        Flyway flyway = Flyway.configure()
-                .dataSource(dataSource)
-                .locations("db/migration")
-                .placeholders(Collections.singletonMap("defaultMaxBorrowTimeInDays", getDefaultMaxBorrowTimeInDays()))
-                .load();
-        flyway.migrate();
-        return flyway;
     }
 
 }
--- src/main/resources/db/migration/V5__Сhange_table_books_add_max_borrow_time_column.sql
+++ src/main/resources/db/migration/V5__Сhange_table_books_add_max_borrow_time_column.sql
@@ -1,4 +1,2 @@
 ALTER TABLE books
 ADD COLUMN max_borrow_time_in_days INT;
-UPDATE books SET max_borrow_time_in_days = ${defaultMaxBorrowTimeInDays};
-
