ab601a78af691e4eff0e3cd674bb57c48616149a
djmolot <djmolot@gmail.com>
2023-07-11 17:20:58 +0200

    tested usage of class AppConfig in WelcomeController

--- src/main/java/company/name/library/controllers/WelcomeController.java
+++ src/main/java/company/name/library/controllers/WelcomeController.java
@@ -1,10 +1,12 @@
 package company.name.library.controllers;
 
+import company.name.library.config.AppConfig;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.media.Content;
 import io.swagger.v3.oas.annotations.media.Schema;
 import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 import java.time.LocalDate;
@@ -14,8 +16,10 @@ import java.util.LinkedHashMap;
 
 import static company.name.library.controllers.ApiDocExamples.WELCOME_OBJECT;
 
+@RequiredArgsConstructor
 @RestController
 public class WelcomeController {
+    private final AppConfig appConfig;
 
     @Operation(summary = "Sends welcome request to server")
     @ApiResponses(value = {
@@ -30,6 +34,7 @@ public class WelcomeController {
         response.put("message", "Welcome to the library!");
         response.put("currentDate", LocalDate.now().format(DateTimeFormatter.ofPattern("dd.MM.yyyy")));
         response.put("currentTime", LocalTime.now().format(DateTimeFormatter.ISO_LOCAL_TIME));
+        response.put("DefaultMaxBorrowTimeInDays", appConfig.getDefaultMaxBorrowTimeInDays());
         return response;
     }
 }
