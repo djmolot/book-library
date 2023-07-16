3c094d42654cd98bdd931665e53b046b1fa0bd63
djmolot <djmolot@gmail.com>
2023-07-16 20:13:31 +0200

    added methods validateBookHoldingPeriodInBooksOfReader & validateReaderAge and used them in metod borrowBookToReader

--- src/main/java/company/name/library/service/LibraryServiceImpl.java
+++ src/main/java/company/name/library/service/LibraryServiceImpl.java
@@ -7,9 +7,10 @@ import company.name.library.repositories.BookRepository;
 import company.name.library.repositories.ReaderRepository;
 import lombok.RequiredArgsConstructor;
 import java.time.LocalDate;
+import java.time.Period;
+import java.time.temporal.ChronoUnit;
 import java.util.List;
 import java.util.Optional;
-
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 
@@ -19,8 +20,6 @@ public class LibraryServiceImpl implements LibraryService {
     private final BookRepository bookRepository;
     private final ReaderRepository readerRepository;
 
-    @Value("${library.defaultMaxBorrowTimeInDays}")
-    private int defaultMaxBorrowTimeInDays;
     @Value("${library.maxNumberOfBooksToBorrow}")
     private int maxNumberOfBooksToBorrow;
     @Value("${library.minAgeOfReaderForRestricted}")
@@ -59,6 +58,10 @@ public class LibraryServiceImpl implements LibraryService {
                         + " does not exist in DB.")
         );
         validateNumberOfBorrowedBooksByReader(reader);
+        validateBookHoldingPeriodInBooksOfReader(reader);
+        if(book.getRestricted()) {
+            validateReaderAge(reader);
+        }
         book.getReader().ifPresent(
                 r -> {
                     throw new ServiceLayerException("Book with ID " + bookId
@@ -106,6 +109,33 @@ public class LibraryServiceImpl implements LibraryService {
         } else {
             throw new ServiceLayerException("Reader has already borrowed " + booksBorrowed +
                     " books, maximum number is " + maxNumberOfBooksToBorrow);
+        }
+    }
+
+    private void validateBookHoldingPeriodInBooksOfReader(Reader reader) {
+        List<Book> booksOfReader = reader.getBooks();
+        LocalDate now = LocalDate.now();
+        for (Book book : booksOfReader) {
+            Optional<LocalDate> borrowDateOptional = book.getBorrowDate();
+            if (borrowDateOptional.isPresent()) {
+                LocalDate borrowDate = borrowDateOptional.get();
+                long holdingPeriod = ChronoUnit.DAYS.between(borrowDate, now);
+                int maxBorrowTimeInDays = book.getMaxBorrowTimeInDays();
+                if (holdingPeriod > maxBorrowTimeInDays) {
+                    throw new ServiceLayerException("Book " + book.getTitle() + " holding period " + holdingPeriod +
+                            " exceeds maximum borrow time in days " + maxBorrowTimeInDays);
+                }
+            }
+        }
+    }
+
+    private void validateReaderAge(Reader reader) {
+        LocalDate currentDate = LocalDate.now();
+        LocalDate readerBirthDate = reader.getBirthDate();
+        int readerAge = Period.between(readerBirthDate, currentDate).getYears();
+        if (readerAge < minAgeOfReaderForRestricted) {
+            throw new ServiceLayerException("Reader id:" + reader.getId() + " " + reader.getName() +
+                    " age:" + readerAge + " is not old enough to borrow restricted books");
         }
     }
 
