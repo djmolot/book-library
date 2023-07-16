958fa8f2f01d3e2b6fd68e7bc863ca0a1de7681c
djmolot <djmolot@gmail.com>
2023-07-16 14:09:15 +0200

    deleted class AppConfig added properties in file application.yml & tested them in class WelcomeController

--- src/main/java/company/name/library/controllers/WelcomeController.java
+++ src/main/java/company/name/library/controllers/WelcomeController.java
@@ -1,12 +1,12 @@
 package company.name.library.controllers;
 
-import company.name.library.config.AppConfig;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.media.Content;
 import io.swagger.v3.oas.annotations.media.Schema;
 import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 import java.time.LocalDate;
@@ -19,7 +19,14 @@ import static company.name.library.controllers.ApiDocExamples.WELCOME_OBJECT;
 @RequiredArgsConstructor
 @RestController
 public class WelcomeController {
-    private final AppConfig appConfig;
+
+    @Value("${library.defaultMaxBorrowTimeInDays}")
+    private int defaultMaxBorrowTimeInDays;
+    @Value("${library.maxNumberOfBooksToBorrow}")
+    private int maxNumberOfBooksToBorrow;
+    @Value("${library.minAgeOfReaderForRestricted}")
+    private int minAgeOfReaderForRestricted;
+
 
     @Operation(summary = "Sends welcome request to server")
     @ApiResponses(value = {
@@ -29,12 +36,14 @@ public class WelcomeController {
             )
     })
     @GetMapping("/api/v1/welcome")
-    public LinkedHashMap<String, String> sayWelcome() {
-        LinkedHashMap<String, String> response = new LinkedHashMap<>();
+    public LinkedHashMap<String, Object> sayWelcome() {
+        LinkedHashMap<String, Object> response = new LinkedHashMap<>();
         response.put("message", "Welcome to the library!");
         response.put("currentDate", LocalDate.now().format(DateTimeFormatter.ofPattern("dd.MM.yyyy")));
         response.put("currentTime", LocalTime.now().format(DateTimeFormatter.ISO_LOCAL_TIME));
-        response.put("DefaultMaxBorrowTimeInDays", appConfig.getDefaultMaxBorrowTimeInDays());
+        response.put("defaultMaxBorrowTimeInDays", defaultMaxBorrowTimeInDays);
+        response.put("maxNumberOfBooksToBorrow", maxNumberOfBooksToBorrow);
+        response.put("minAgeOfReaderForRestricted", minAgeOfReaderForRestricted);
         return response;
     }
 }
